3d8cc79279e125e8ce6f1eca6a4e865e04a0c08f
Pablo <Pablo@Pablo-PC>
2021-07-07 20:00:21 -0500

    MainE1

--- src/Diagramas_de_flujo/MainE1.java
+++ src/Diagramas_de_flujo/MainE1.java
@@ -7,6 +7,20 @@ package Diagramas_de_flujo;
  * @author Pablo
  *
  */
+import java.util.Scanner;
 public class MainE1 {
 
+	public static void main(String[]arg) {
+		System.out.println("Ingresar el valor del sueldo");
+		Scanner teclado=new Scanner(System.in);
+		int S= teclado.nextInt();
+		
+		if (S<300) {
+			int B=40;
+			System.out.println("Su bonificación es"+B);
+		}else{
+			int B=0;
+			System.out.println("Su bonificación es"+B);
+		}
+	}  
 }
