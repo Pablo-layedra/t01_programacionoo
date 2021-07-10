45c35edfd0eb135766459cd017f226cc7b02b1d8
Pablo <Pablo@Pablo-PC>
2021-07-09 19:52:45 -0500

    Tabla del 7

--- src/Diagramas_de_flujo/MainE4.java
+++ src/Diagramas_de_flujo/MainE4.java
@@ -1,16 +1,17 @@
 package Diagramas_de_flujo;
 
-
 import java.util.Scanner;
+
 public class MainE4 {
-	
-		// Imprimir en pantalla la tabla de multiplicar del 7 hasta el 12
-		
-		public static void main(String[] args) {
-			System.out.println("Imprimir labla de multiplicar del 7 hasta el 12:");
-			for (int M=0; M<=12; M=(M++)) {
-				System.out.println("7*"+M);
-			}
-			System.out.println("Tabla del 7 impresa");
+
+	// Imprimir en pantalla la tabla de multiplicar del 7 hasta el 12
+
+	public static void main(String[] args) {
+		System.out.println("Imprimir labla de multiplicar del 7 hasta el 12:");
+		for (int M=0; M<=12; M = M+1) {
+			int Mul=(M*7);
+			System.out.println("7*"+M+"="+Mul);
 		}
+		System.out.println("Tabla del 7 impresa");
 	}
+}
