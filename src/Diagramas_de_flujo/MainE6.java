f293ee197dea7ab4591556c492b6a60bb3ef9485
Pablo <Pablo@Pablo-PC>
2021-07-14 19:12:55 -0500

    Taller 3 y 4

---
+++ src/Diagramas_de_flujo/MainE6.java
@@ -0,0 +1,22 @@
+package Diagramas_de_flujo;
+
+import java.util.Scanner;
+
+public class MainE6 {
+	
+   public static void main(String[] args) {
+		
+		System.out.println("Ingrese número de filas:");
+		Scanner teclado=new Scanner(System.in);
+		int N= teclado.nextInt();
+		
+		for (int Fila = 1; Fila <= N; Fila = Fila + 1) {
+			
+			for (int ast=N; ast>=Fila; ast=ast-1){
+				System.out.print("*");
+			}
+			System.out.println();	
+		}	
+		}
+   }
+
