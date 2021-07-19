ace982a4bc6e8781456993b56435bc9f22f09853
Pablo <Pablo@Pablo-PC>
2021-07-19 15:12:31 -0500

    Hello

---
+++ src/Diagramas_de_flujo/MainE3.java
@@ -0,0 +1,16 @@
+package Diagramas_de_flujo;
+
+import java.util.Scanner;
+
+public class MainE3 {
+	// Imprimir en pantalla los 10 primeros numeros impares
+	
+	public static void main(String[] args) {
+		System.out.println("Imprimir los numeros impares:");
+		for (int I = 1; I < 20; I = I + 2) {
+			System.out.println("El siguiente numero es:" + I);
+			
+		}
+		System.out.println("Numeron impares impresos");
+	}
+}
