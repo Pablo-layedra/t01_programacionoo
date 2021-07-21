7553f7167397309a1e5fc96423ebf35589d8ce3e
Pablo-layedra <pmlayedra@uce.edu.ec>
2021-07-21 18:58:30 -0500

    Add files via upload

---
+++ src/tipos_de_datos/MainTiposDeDatos1.java
@@ -0,0 +1,17 @@
+package tipos_de_datos;
+
+public class MainTiposDeDatos1 {
+
+	public static void main(String[]arg) {
+		
+		short CantE=1500;
+		byte EntradaCircuito=001;
+		int CantCiu=17000;
+		long Habitantes=160000000;
+		float Sueldo= 150.25F;
+		double CuentaB = 156700.35; 
+		char OpcionMenu='A';
+		boolean resultado = true; 
+		
+	}
+}
