d6a9e2d013cde609eeb540b5a451b9bf9d50e21f
Pablo <Pablo@Pablo-PC>
2021-07-20 20:10:24 -0500

    Prueba

--- src/Diagramas_de_flujo/Prueba.java
+++ src/Diagramas_de_flujo/Prueba.java
@@ -2,4 +2,15 @@ package Diagramas_de_flujo;
 
 public class Prueba {
 
+	// Imprimir en pantalla la tabla de multiplicar del 7 hasta el 12
+
+	public static void main(String[] args) {
+		System.out.println("Imprimir labla de multiplicar del 7 hasta el 12:");
+		for (int M=0; M<=12; M = M+1) {
+			int Mul=(M*7);
+			System.out.println("7*"+M+"="+Mul);
+		}
+		System.out.println("Tabla del 7 impresa");
+		
+	}
 }
