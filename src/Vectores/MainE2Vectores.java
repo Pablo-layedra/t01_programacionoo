5c6111a1e842baf4ada86ddcebe5f706c70978d9
Pablo <Pablo@192.168.1.5>
2021-08-03 19:53:08 -0500

    Ejercicios Vectores

--- src/Vectores/MainE2Vectores.java
+++ src/Vectores/MainE2Vectores.java
@@ -6,7 +6,19 @@ public class MainE2Vectores {
 	
 	public static void main(String[]arg) {
 		
+		char Vector[]= new char[3];
 		
+		for (char i=0; i<3; i++){
+			System.out.println("Ingresar valor de la posicion:"+i);
+			
+			Scanner lector=new Scanner(System.in);
+			char N= lector.next().charAt(0);
+			Vector[i]=N;
+		}
+		//recorriendo el vector lleno
+		for (int i=0; i<3; i++){
+			System.out.println("Valor de la posicion:"+i);
+			System.out.println(Vector[i]);
+		}
 	}
-
 }
