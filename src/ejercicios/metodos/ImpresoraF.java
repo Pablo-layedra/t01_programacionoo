e2f8d81e81fe30bfb310df451b34231c23895e08
jf7ch <jf7ch@DESKTOP-VBLNR4T>
2021-08-13 10:43:47 -0500

    Clase Impresora.

---
+++ src/ejercicios/metodos/ImpresoraF.java
@@ -0,0 +1,18 @@
+
+public class ImpresoraF {
+public String color;
+	
+	public ImpresoraF() {
+		
+	}
+	
+	public void imprimir(String datosFactura, int letra) {
+	System.out.println("Logo Ferreteria");
+	System.out.println("**************");
+	System.out.println(datosFactura);
+	System.out.println("**************");
+	System.out.println("Pie de firma");
+	
+	}
+
+}
