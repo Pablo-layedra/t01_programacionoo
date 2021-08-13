ba5873635a009aa25b7281c354902a90a787410c
Pablo <Pablo@192.168.1.5>
2021-08-13 11:53:20 -0500

    Notas 2

---
+++ src/ejercicios/metodos/Notas2.java
@@ -0,0 +1,13 @@
+package ejercicios.metodos;
+
+public class Notas2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Impresora Impresión1= new Impresora(); //Instanciar la clase
+		Impresión1.Imprimir("Promedio Programación 1:20; Promedio Investigación: 17",12);
+
+	}
+
+}
