012b390f8a2002287473df34871d1c9bc124bde0
Pablo <Pablo@Pablo-PC>
2021-08-25 19:40:47 -0500

    25/08/2021

--- src/modificadores/acceso/PacienteTerceraEdad.java
+++ src/modificadores/acceso/PacienteTerceraEdad.java
@@ -5,8 +5,6 @@ public class PacienteTerceraEdad extends Paciente {
 	public void examenProstata() {
 		System.out.println("Nombre: "+nombre+apellido);
 		System.out.println("Diagnóstico: "+diagnostico);
-		System.out.println("Cedula: "+cedula);
-		//Debido a que es un atributo private, no aplica para subclases
 		RealizarDiagnostico();
 		
 	}
